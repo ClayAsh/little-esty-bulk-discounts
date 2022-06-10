7962ff3d7e137bf59c957d4687d31335c38682bb
Clay Ash <claycourtneyash@gmail.com>
2022-06-09 20:28:58 -0600

    Add new and create methods to discounts controller, add new form and tests for form

---
+++ spec/features/discounts/new_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'discount new page' do 
+  let!(:merchant_1) { Merchant.create(name: "Franks Fun Stuff") }
+  let!(:discount_1) { merchant_1.discounts.create(percentage: 20 , quantity: 10 ) }
+  let!(:discount_2) { merchant_1.discounts.create(percentage: 30 , quantity: 20 ) }
+  let!(:discount_3) { merchant_1.discounts.create(percentage: 50 , quantity: 40 ) }
+
+  it 'can add a new discount' do 
+    visit merchant_discounts_path(merchant_1.id)
+
+    click_link "Create New Discount"
+
+    expect(current_path).to eq(new_merchant_discount_path(merchant_1.id))
+
+    fill_in "quantity", with: "10"
+    fill_in "percentage", with: "10"
+    click_button "Submit"
+
+    expect(current_path).to eq(merchant_discounts_path(merchant_1.id))
+    expect(page).to have_content("Buy 10 get 10% off")
+  end 
+end
