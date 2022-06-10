510b5070420a8e3af24064fffcc3a7f00a1d6964
Clay Ash <claycourtneyash@gmail.com>
2022-06-09 19:57:11 -0600

    Add relationships to models for discount table

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,10 @@
 class Transaction < ApplicationRecord
-  validates_presence_of :invoice_id, :credit_card_number, :result
-  belongs_to :invoice
   enum result: { success: 0, failed: 1 }
+  validates_presence_of :invoice_id, :credit_card_number, :result
+
+  belongs_to :invoice
+  has_many :invoice_items, through: :invoice 
+  has_many :items, through: :invoice_items 
+  has_many :merchants, through: :items 
+  has_many :discounts, through: :merchants  
 end
